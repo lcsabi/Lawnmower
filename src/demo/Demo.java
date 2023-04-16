ca7831f23ec272694006409ac23580fa5ed1e88a
Laszlo Csaba <devcsabi@gmail.com>
2023-04-17 01:12:31 +0200

    Add demo run with a 5 by 5 garden.

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,6 +1,15 @@
 package demo;
 
+import component.Garden;
+
+// add stopwatch -> Timer, TimerTask
+// simulate time: different for moving and cutting (based on grass length)
+// add logging to console as well as to a text file -> SLF4J API -> src/resources/logback.xml
+
 public class Demo {
 
-
+    public static void main(String[] args) {
+        Garden garden = new Garden(5, 5);
+        garden.run();
+    }
 }
