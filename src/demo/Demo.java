f86fd67c377232f763e664e7b0a70a6b997c1b09
Laszlo Csaba <devcsabi@gmail.com>
2023-04-17 01:45:08 +0200

    Add TODO.

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,6 +2,7 @@ package demo;
 
 import component.Garden;
 
+// document with JavaDoc
 // add stopwatch -> Timer, TimerTask
 // simulate time: different for moving and cutting (based on grass length)
 // add logging to console as well as to a text file -> SLF4J API -> src/resources/logback.xml
