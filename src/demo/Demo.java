683ab83044622ad6fb143a95244b665df0c9581c
Laszlo Csaba <devcsabi@gmail.com>
2023-04-17 10:57:42 +0200

    Move TODOs to GitHub Issues.

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,12 +2,6 @@ package demo;
 
 import component.Garden;
 
-// document with JavaDoc
-// add stopwatch -> Timer, TimerTask
-// simulate time: different for moving and cutting (based on grass length)
-// add logging to console as well as to a text file -> SLF4J API -> src/resources/logback.xml
-// make into JavaFX
-
 public class Demo {
 
     public static void main(String[] args) {
