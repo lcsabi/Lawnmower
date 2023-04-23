74b6c8269bee325d0e63335e90e980c4232520ee
László Csaba <laszlo.cs4ba@gmail.com>
2023-04-23 20:42:30 +0200

    feat: WeatherAPI
    - add ConfigReader to read config.properties file, include it in .gitignore
    - implement WeatherAPI.com REST API connection
    - add GSON to the project to handle API response

---
+++ src/main/java/util/ConfigReader.java
@@ -0,0 +1,37 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private final Properties properties;
+    private final String configFileName = "src/main/resources/config.properties";
+
+    public ConfigReader() {
+        properties = new Properties();
+        try (InputStream in = new FileInputStream(configFileName)) {
+            properties.load(in);
+        } catch (IOException e) {
+            System.out.println("Configuration file not found.");
+        }
+    }
+
+    public String getAPILink() {
+        return properties.getProperty("api_link");
+    }
+
+    public String getAPIKey() {
+        return properties.getProperty("api_key");
+    }
+
+    public String getCity() {
+        return properties.getProperty("city");
+    }
+
+    public String getParameter() {
+        return properties.getProperty("parameter");
+    }
+}
